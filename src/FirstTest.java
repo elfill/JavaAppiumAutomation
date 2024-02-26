af78b9f290a33baa5ba975c28881ec3e0b5af0e3
Rukhlov IVan <70105799+elfill@users.noreply.github.com>
2024-02-26 19:05:47 +0300

    Eding existing tests

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -696,43 +696,7 @@ public class FirstTest {
 
     }
 
-        @Test
-        public void testForSearchTitle()
-    {
 
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
-                "Cannot find 'Skip' button",
-                5
-        );
-
-        waitForElementAndClick(
-                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
-                "Cannot find 'Search Wikipedia' input",
-                5
-        );
-
-        waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
-                "Java",
-                "Cannot find 'Search Wikipedia' input",
-                5
-        );
-
-        waitForElementAndClick(
-                By.xpath("//*[@resource-id = 'org.wikipedia:id/fragment_search_results']//*[@text = 'Object-oriented programming language']"),
-                "Cannot find 'Object-oriented programming language' topic searching by Java",
-                5
-        );
-
-        assertElementPresent(
-                By.id("pcs-edit-section-title-description"),
-                "text",
-                "Object-oriented programming language",
-                "Element is not found",
-                5
-        );
-    }
 
     //методы
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
@@ -855,14 +819,4 @@ public class FirstTest {
         return  element.getAttribute(attribute);
     }
 
-    private void assertElementPresent (By by, String attribute, String expected_result, String error_message, long timeoutInSeconds)
-    {
-        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-        String actual_result = element.getAttribute(attribute);
-
-        Assert.assertTrue(
-                error_message,
-                actual_result.equals(expected_result)
-        );
-    }
 }
